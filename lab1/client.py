30916245c94a3a0d383fb51dafed0d0b676fc263
JuliaTodorova <juliastoycheva@gmail.com>
2016-04-18 00:43:50 -0700

    Added functionality that packages two integers per byte

--- lab1/client.py
+++ lab1/client.py
@@ -2,22 +2,36 @@ import sys, socket
 
 server = sys.argv[1]
 port = int(sys.argv[2])
+operation = sys.argv[3]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 s.connect((server,port))
 
-data = bytearray(4)
+data = bytearray(7)
 
-if sys.argv[3] == "+":
+if operation == "+":
 	data[0] = 2**0
-elif sys.argv[3] == "-":
+elif operation == "-":
 	data[0] = 2**1
-elif sys.argv[3] == "*":
+elif operation == "*":
 	data[0] = 2**2
 
-data[1] = int(sys.argv[4])
-data[2] = int(sys.argv[5])
+MAX_LEN = 14
+
+if len(sys.argv) <= MAX_LEN:
+	count = len(sys.argv) - 4
+else
+	count = MAX_LEN - 4
+
+j = 2
+for i in range(4, count):
+	if(i % 2 = 0){
+		data[j] = (int(sys.argv[i])) << 4
+	}
+	else
+		data[j] = data[j] | sys.argv[i]
+		j = j + 1
 
 s.sendall(data)
 
