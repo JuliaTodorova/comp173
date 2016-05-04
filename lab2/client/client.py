a4e9381bce1304f085f075ef8d8bb1636479da2e
JuliaTodorova <juliastoycheva@gmail.com>
2016-05-04 12:06:29 -0700

    Added get functionality

--- lab2/client/client.py
+++ lab2/client/client.py
@@ -14,35 +14,55 @@ readyC = s.recv(1024).decode("UTF-8")
 if command ==  "GET":
 	s.send(("GET"+filename).encode("UTF-8"))
 
+	error = s.recv(1024).decode("UTF-8")
+	if error[0:5] == "ERROR":
+		print(error)
+		exit(0)
+	else:
+		pass
+
 	data = s.recv(1024)
 	size = int.from_bytes(data, byteorder='big', signed=False)
-	
+
+	print("client receiving file " + filename + " ("+ str(size) +" bytes)")
+
 	f = open(filename, "wb")
 	while size > 0:
-		data = s.recv(1024)
+		data = s.recv(min(size, 1024))
 		f.write(data)
 		size -= len(data)
 	f.close()
-	data = s.recv(1024)
+	data = s.recv(min(size, 1024))
+	done = s.recv(8).decode("UTF-8")
+	print(done)
+	#if done:
+		#print("Complete")
 	s.close()
 
 elif command == "PUT":
-	s.send(("PUT"+filename).encode("UTF-8"))
 
-	if not os.access(filename, os.R_OK) or not os.path.isfile(filename):
-		print("No access to file: " + filename)
+	s.send(("PUT "+filename).encode("UTF-8"))
 
-	size = os.path.getsize(filename)
-	s.send(size.to_bytes(8, byteorder='big', signed=False))
-	f = open(filename, "rb")
-	
-	while size > 0:
-		data = f.read(1024)
-		s.send(data)
-		size -= len(data)
-	f.close()
-	s.close()
-	
+	okC = s.recv(1024).decode("UTF-8")
+
+	if os.access(filename, os.R_OK):
+		size = os.path.getsize(filename)
+		print("client sending file " + filename + " ("+ str(size) +" bytes)")
+		s.send(size.to_bytes(8, byteorder='big', signed=False))
+
+		numBytes = s.recv(1024).decode("UTF-8")
+
+		f = open(filename, "rb")
+		
+		while size > 0:
+			data = f.read(1024)
+			s.send(data)
+			size -= len(data)
+		f.close()
+		s.close()
+	elif not os.access(filename, os.R_OK) or not os.path.isfile(filename):
+		print("does not exist”" + filename)
+
 elif command == "DEL":
-	s.send(("DEL"+filename).encode("UTF-8"))
+	s.send(("DEL "+filename).encode("UTF-8"))
 	s.close()
--- lab2/server/server.py
+++ lab2/server/server.py
@@ -4,7 +4,7 @@ port = int(sys.argv[1])
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind( ("", port) )
-s.listen(3)
+s.listen(0)
 
 while True:
 	conn, address = s.accept()
@@ -13,24 +13,34 @@ while True:
 	data = conn.recv(1024).decode("UTF-8")
 	command = data[0:3]
 	filename = data[3:]
-
 	if command == "GET":
+		if os.access(filename, os.R_OK):
+			conn.send("OK".encode("UTF-8"))
+			
+			size = os.path.getsize(filename)
+			conn.send(size.to_bytes(1024, byteorder='big', signed=False))
+			f = open(filename, "rb")
 
-		size = os.path.getsize(filename)
-		conn.send(size.to_bytes(8, byteorder='big', signed=False))
-		f = open(filename, "rb")
+			while size > 0:
+				data = f.read(1024)
+				conn.send(data)
+				size -= len(data)
+			f.close()
+			conn.send("DONE".encode("UTF-8"))
+			conn.close()
 
-		while size > 0:
-			data = f.read(1024)
-			conn.send(data)
-			size -= len(data)
-		f.close()
-		conn.close()
+		elif (not os.path.isfile(filename)):
+			conn.send(("ERROR: "+filename+" does not exist").encode("UTF-8"))
+			conn.close()
 
 	elif command == "PUT":
+		conn.send("OK".encode("UTF-8"))
+		
 		data = conn.recv(1024)
 		size = int.from_bytes(data, byteorder='big', signed=False)
-		
+		print(size)
+		conn.send("OK".encode("UTF-8"))
+
 		f = open(filename, "wb")
 		while size > 0:
 			data = conn.recv(1024)
@@ -38,8 +48,10 @@ while True:
 			size -= len(data)
 		f.close()
 		data = conn.recv(1024)
+		conn.send("DONE".encode("UTF-8"))
 		conn.close()
 
 	elif command == "DEL":
 		if os.path.exists(filename):
 			os.remove(filename)
+		conn.close()
