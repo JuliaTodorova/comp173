eff2945abb0d5e0d0716b556eb7f0484f4c6476a
JuliaTodorova <juliastoycheva@gmail.com>
2016-05-16 21:54:15 -0700

    Fixed get request

--- lab3/client.py
+++ lab3/client.py
@@ -11,8 +11,7 @@ else:
 
 s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s1.connect( (webhost, 80) )
-s1.send(("GET " + resource + " HTTP/1.1\n").encode("UTF-8"))
-s1.send(("Host: " + webhost + "\n\n").encode("UTF-8"))
+s1.send(("GET " + resource + " HTTP/1.1\n" + "Host: " + webhost + "\n\n").encode("UTF-8"))
 
 s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s2.connect( ("rtvm.cs.camosun.bc.ca", 10010) )
@@ -32,25 +31,26 @@ while (state != 4):
 			
 			if ("</HTML>" in (previous).upper()):
 				tag = previous.upper().index("</HTML>")
-				sendTo = previous[:tag + 7]
-				s2.send(sendTo.encode("UTF-8"))
+				send = previous[:tag + 7]
+				s2.send(send.encode("UTF-8"))
 				state = 3
 				previous = ""
 		else:
 			previous = current
+			
 	elif (state == 2): 
 			current = s1.recv(1024).decode("UTF-8")
 			if ("</HTML>" in (previous + current).upper()):
 				tag = (previous+current).upper().index("</HTML>")
-				sendTo = (previous+current)[:tag + 7]
-				s2.send(sendTo.encode("UTF-8"))
+				send = (previous+current)[:tag + 7]
+				s2.send(send.encode("UTF-8"))
 				state = 3
 				previous = ""
 			else:
 				s2.send(previous.encode("UTF-8"))
 				previous = current
 	elif (state == 3): 
-		current = s2.recv(1024).decode("UTF-8")
+		current = s2.recv(1024).decode("ASCII")
 		if ("COMP173" in (previous+current).upper()):
 			tag = (previous+current).upper().index("COMP173")
 			print((previous+current)[:tag], end='')
